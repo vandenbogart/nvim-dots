515b82436b5d15ddf1f3a38fd6856f9a61fc58cd
Eric Bogard <ebo@testmachine.ai>
2024-06-29 14:11:28 -0700

    changes

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -7,12 +7,24 @@ return {
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
         'L3MON4D3/LuaSnip',
+        'onsails/lspkind.nvim'
 
     },
     opts = function()
+        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+        vim.opt.shortmess:append "c"
         -- vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
         local cmp = require("cmp")
-        local defaults = require("cmp.config.default")()
+        local ok, lspkind = pcall(require, "lspkind")
+        if not ok then
+            return
+        end
+
+        lspkind.init {
+            symbol_map = {
+                Copilot = "",
+            },
+        }
         return {
             completion = {
                 completeopt = "menu,menuone,noinsert",
@@ -40,27 +52,63 @@ return {
                 end,
             }),
             sources = cmp.config.sources({
+                { name = "nvim_lua" },
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "path" },
             }, {
-                { name = "buffer" },
+                { name = "buffer", keyword_length = 5 },
             }),
-            --formatting = {
-            --    format = function(_, item)kk
-            --         local icons = require("lazyvim.config").icons.kinds
-            --         if icons[item.kind] then
-            --             item.kind = icons[item.kind] .. item.kind
-            --         end
-            --         return item
-            --    end,
-            --},
-            -- experimental = {
-            --     ghost_text = {
-            --         hl_group = "CmpGhostText",
-            --     },
-            -- },
-            sorting = defaults.sorting,
+            sorting = {
+                -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+                comparators = {
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+
+                    -- copied from cmp-under, but I don't think I need the plugin for this.
+                    -- I might add some more of my own.
+                    function(entry1, entry2)
+                        local _, entry1_under = entry1.completion_item.label:find "^_+"
+                        local _, entry2_under = entry2.completion_item.label:find "^_+"
+                        entry1_under = entry1_under or 0
+                        entry2_under = entry2_under or 0
+                        if entry1_under > entry2_under then
+                            return false
+                        elseif entry1_under < entry2_under then
+                            return true
+                        end
+                    end,
+
+                    cmp.config.compare.kind,
+                    cmp.config.compare.sort_text,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
+            },
+            formatting = {
+                -- Youtube: How to set up nice formatting for your sources.
+                format = lspkind.cmp_format {
+                    with_text = true,
+                    menu = {
+                        buffer = "[buf]",
+                        nvim_lsp = "[LSP]",
+                        nvim_lua = "[api]",
+                        path = "[path]",
+                        luasnip = "[snip]",
+                        gh_issues = "[issues]",
+                        tn = "[TabNine]",
+                        eruby = "[erb]",
+                    },
+                },
+            },
+            experimental = {
+                -- I like the new menu better! Nice work hrsh7th
+                native_menu = false,
+
+                -- Let's play with this for a day or two
+                ghost_text = false,
+            },
         }
     end,
     config = function(_, opts)
