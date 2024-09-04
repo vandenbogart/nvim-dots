3e92e0776768e72e1a44ed04d13fda37530d358f
Eric Bogard <ebo@testmachine.ai>
2024-09-04 12:09:55 -0400

    stuff

--- nvim/lua/plugins/autoclose.lua
+++ nvim/lua/plugins/autoclose.lua
@@ -1,4 +1,5 @@
 return {
+    enabled=false,
     'altermo/ultimate-autopair.nvim',
     event={'InsertEnter','CmdlineEnter'},
     branch='v0.6', --recommended as each new version will have breaking changes
--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -11,6 +11,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -28,6 +29,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "no"
 vim.opt.foldcolumn = "1"
+vim.opt.colorcolumn = "80"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
